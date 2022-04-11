c1454645c5fe7071b566e8d5115ec91ab81da72d
MrCiffa <davideams98@gmail.com>
2022-04-11 16:35:16 +0200

    test

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-import {loadGLTF, loadVideo} from "./libs/loader.js";
+import {loadGLTF, loadVideo} from "./testing/libs/loader.js";
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: './assets/targets/RCS.mind',
+      imageTargetSrc: './testing/assets/targets/RCS.mind',
     });
     const {renderer, scene, camera} = mindarThree;
 
-    const video = await loadVideo("./assets/videos/RCS.mp4");
+    const video = await loadVideo("./testing/assets/videos/RCS.mp4");
     const texture = new THREE.VideoTexture(video);
 
     const geometry = new THREE.PlaneGeometry(1, 204/480);
