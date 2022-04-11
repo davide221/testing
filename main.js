c058bfa90f4815b818f32eae2d61e5ad3103f27e
MrCiffa <davideams98@gmail.com>
2022-04-11 16:31:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: '../../assets/targets/RCS.mind',
+      imageTargetSrc: './assets/targets/RCS.mind',
     });
     const {renderer, scene, camera} = mindarThree;
 
-    const video = await loadVideo("../../assets/videos/RCS.mp4");
+    const video = await loadVideo("./assets/videos/RCS.mp4");
     const texture = new THREE.VideoTexture(video);
 
     const geometry = new THREE.PlaneGeometry(1, 204/480);
